c3830d13f1f7aebcc6cfab741e47464c2d8ebf6c
Mika Di Re <mikadire@hotmail.co.uk>
2025-10-03 15:17:36 +0200

    bug fix: boot.sh inncorrect gunicorn start command

--- boot.sh
+++ boot.sh
@@ -7,4 +7,4 @@ while true; do
 	echo Upgrade command failed, retrying in 5 secs...
 	sleep 5
 done
-exec gunicorn -b :5000 -w 3 --access-logfile /dev/null - --error-logfile - app:app
+exec gunicorn -b :5000 -w 3 --access-logfile /dev/null --error-logfile - app:app
